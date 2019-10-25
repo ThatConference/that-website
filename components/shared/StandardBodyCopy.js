6414b9a0389152c8ad7494cf3dc5873f969f885d
Keith Burnell <keith@kburnell.com>
2019-10-25 17:56:05 -0500

    WIP: Partner

---
+++ components/shared/StandardBodyCopy.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StandardBodyCopy = styled.p`
+  font-size: 14pt;
+  font-family: 'Open Sans', sans-serif;
+  padding-left: 20px;
+`;
+
+export default StandardBodyCopy;
