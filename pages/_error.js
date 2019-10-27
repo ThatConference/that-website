be97c4e5b0ac065ce07e6072792c5b3a322a5cf3
Clark Sell <clark.sell@csell.net>
2019-10-27 11:19:52 -0500

    updating to drop more logging info

--- pages/_error.js
+++ pages/_error.js
@@ -3,6 +3,9 @@ import sentry from '../lib/sentry';
 
 function Error({ statusCode }) {
   const { Sentry } = sentry();
+  console.log('this is my sentry', Sentry);
+  console.log('dsn', process.env.SENTRY_DSN);
+
   Sentry.captureEvent('this is an event');
   Sentry.captureException({ message: 'broke' });
   Sentry.captureMessage('making a message');
