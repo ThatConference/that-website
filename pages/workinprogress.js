afeb7debf49f243eb59df29bfa281cf5766b6909
Aaron Douglas <astralbodies@gmail.com>
2019-10-27 14:25:39 -0500

    Added new page for telling people the site is a WIP.

---
+++ pages/workinprogress.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import ContentSection from '../components/shared/ContentSection';
+
+const WorkInProgress = ({ className }) => {
+    return (
+      <ContentSection className={className}>
+          This is where the content goes.
+      </ContentSection>
+    );
+  };
+  
+  export default WorkInProgress;
+  
