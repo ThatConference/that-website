e1f080d9a2956ef1078fb62bab86709a85e3ba5e
Sara Gibbons <saragibby@gmail.com>
2019-11-29 19:33:53 -0500

    dynamic markdown rendering for copyright page
    base to support additional markdown pages

--- markdown/bacon.md
+++
@@ -1,9 +0,0 @@
-# BACON
-
-**is delicious!**
-
-- [Tickets](variables_Particle)
-- list
-
-One of the partners is variables_Cuna-Mutual-Group
-
--- pages/wi/[markdown].js
+++ pages/wi/[markdown].js
@@ -3,86 +3,88 @@
 import React from 'react';
 import Head from 'next/head';
 import Markdown from 'markdown-to-jsx';
+import fm from 'front-matter';
+import flatten from 'flat';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import Error from '../_error';
 
 import ContentSection from '../../components/shared/ContentSection';
 
-// using this as a pretend query for getting variables from the api
-const GET_PARTNERS = gql`
-  query getPartners {
-    partners {
+// Query for event specific data to use in markdown rendering
+const GET_EVENT = gql`
+  query getEvent($eventId: ID!) {
+    event(id: $eventId) {
       id
-      slug
-      year
-      partnershipLevel
-      companyName
-      companyLogo
-      heroImage
-      website
+      name
+      slogan
+      startDate
+      endDate
+      venue {
+        id
+        name
+        address
+        city
+        state
+        zip
+      }
     }
   }
 `;
 
-// take the array of partner data and return a key-value pair object (just formatting here for whatever we return from the api)
-function formatPartnerDataIntoVariables(data) {
-  return data.reduce(function(result, item) {
-    const reassignmentResult = result;
-    // making the key be of format variables_* so that it's less likely to match incorrectly
-    reassignmentResult[`variables_${item.slug}`] = item.companyName;
-    return reassignmentResult;
-  }, {});
-}
-
-// mapObj should be of the format { eventDate:"10/10/2020", eventLocation:"wi", variable3:"value3" }
-function replaceAll(str, mapObj) {
-  // create regEx with all variables as keys that are pipe delimited, for example "eventDate|eventLocation|variable3"
-  const regularExpression = new RegExp(Object.keys(mapObj).join('|'), 'gi');
+const replaceVariables = (markdownBody, variables) => {
+  // create regEx with all variables as keys that are pipe delimited, for example "event_date|event_name|event_slogan"
+  const regularExpression = new RegExp(Object.keys(variables).join('|'), 'gi');
 
   // do the actual replace searching for regEx matches on keys and replace using the values
-  return str.replace(regularExpression, function(matched) {
-    return mapObj[matched];
+  return markdownBody.replace(regularExpression, matched => {
+    return variables[matched];
   });
-}
+};
 
-const pocMarkdown = ({ markdownContent }) => {
-  // print out original markdownContent
-  console.log(`original md`, markdownContent);
+const RenderedMarkdown = ({ markdownContent, statusCode }) => {
+  if (statusCode) {
+    return <Error statusCode={statusCode} />;
+  }
 
-  // make a call to an API for debugging; pretending this gets variable values
-  const { loading, error, data } = useQuery(GET_PARTNERS);
+  const parsedMarkdown = fm(markdownContent);
+
+  const { loading, error, data } = useQuery(GET_EVENT, {
+    variables: { eventId: 'ByE7Dc7eCGcRFzLhWhuI' }, // WI eventId
+  });
   if (loading) return null;
   if (error) return null;
-  console.log(`variables from api (actually partners)`, data);
 
-  // data from API may not be in format we want
-  const variables = formatPartnerDataIntoVariables(data.partners);
-  console.log(`formatted data (key:value)`, variables);
-
-  // replace variables in the md file with what we got back from the api
-  const markdownTransformed = replaceAll(markdownContent, variables);
-  console.log(`transformed md`, markdownTransformed);
+  const updatedMarkdown = replaceVariables(
+    parsedMarkdown.body,
+    flatten(data, { delimiter: '_' }),
+  );
 
   return (
     <div>
       <Head>
-        <title key="title">POC: Markdown - THAT Conference</title>
+        <title key="title">{parsedMarkdown.attributes.title}</title>
       </Head>
 
       <ContentSection>
-        <h3>
-          POC: Markdown with Variables (really partners) and Dynamic Routing
-        </h3>
-        <Markdown>{markdownTransformed}</Markdown>
+        {parsedMarkdown.attributes.title && (
+          <h2>{parsedMarkdown.attributes.title}</h2>
+        )}
+        <Markdown>{updatedMarkdown}</Markdown>
       </ContentSection>
     </div>
   );
 };
 
-pocMarkdown.getInitialProps = async function(context) {
+RenderedMarkdown.getInitialProps = async function(context) {
   const slug = context.query.markdown;
-  const markdownContent = require(`../../markdown/${slug}.md`).default;
-  return { markdownContent };
+
+  try {
+    const markdownContent = require(`../../markdown/${slug}.md`).default;
+    return { markdownContent };
+  } catch (err) {
+    return { statusCode: 404 };
+  }
 };
 
-export default pocMarkdown;
+export default RenderedMarkdown;
