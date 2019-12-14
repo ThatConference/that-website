25632f7029cf8fb87f7304ea0784a05ef566e700
Clark Sell <clark.sell@csell.net>
2019-12-14 06:56:09 -0600

    adds ac tracking and email signup modal

--- pages/_document.js
+++ pages/_document.js
@@ -29,6 +29,20 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+
+          <script
+            type="text/javascipt"
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+                vgo('setAccount', '610031336');
+                vgo('setTrackByDefault', true);
+
+                vgo('process');
+              `,
+            }}
+          />
           <script
             src="//code.tidio.co/qcwuuigfzw3cjegsc2fyo0sniyh3c3ue.js"
             async
