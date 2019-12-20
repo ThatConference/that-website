197156a510d638b3867b4ab8ac1c0be0351b62a1
Keith Burnell <keith@kburnell.com>
2019-12-20 09:21:42 -0600

    wip

---
+++ components/shared/LinksInput.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const LinksInput = ({ setFieldTouched, setFieldValue, field }) => {
+  return (
+    <div>
+      <input />
+      <input />
+    </div>
+  );
+};
+
+export default LinksInput;
