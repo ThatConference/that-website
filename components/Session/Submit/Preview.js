4c54d4adb0d63e1b49aa9c219d5042e2ef257565
Keith Burnell <keith@kburnell.com>
2019-12-23 06:52:44 -0600

    renamed component to match file

--- components/Session/Submit/Preview.js
+++ components/Session/Submit/Preview.js
@@ -72,7 +72,7 @@ const MarkdownIt = require('markdown-it');
 
 const converter = new MarkdownIt();
 
-const Lastly = () => {
+const Preview = () => {
   const session = {
     title: 'How to kickstart your software development',
     longDescription: `Description of the session goes here. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
@@ -183,4 +183,4 @@ const Lastly = () => {
   );
 };
 
-export default Lastly;
+export default Preview;
