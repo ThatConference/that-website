af8272642fa7c07577091e95cba3efe606c26e42
Mike Cook <mcook@artin.org>
2019-12-23 18:45:20 -0600

    Updated copy on several Call for Counselor pages.

--- components/CounselorAgreement/Commitments.js
+++ components/CounselorAgreement/Commitments.js
@@ -3,17 +3,14 @@ import React from 'react';
 const Agreement = () => {
   return (
     <>
-      <h3>Commitments to THAT</h3>
+      <h3>Commitments to THAT Conference</h3>
       <ul>
         <li>
-          You agree to a code of conduct and a zero tolerance policy for any
-          sort of harassment.
-        </li>
-        <li>You’ll attend the entirety of the conference</li>
-        <li>
-          You’ll promise to meet new people and really discover what it means to
-          be a part of THAT Conference.
-        </li>
+          You agree to the Code of Conduct and a zero tolerance policy for any
+          sort of harassment.</li>
+        <li>You’ll attend the entirety of the conference.</li>
+        <li>You’ll promise to meet new people and really discover what it means to
+          be a part of THAT Conference.</li>
       </ul>
     </>
   );
