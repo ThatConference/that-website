c99ae8a5c5eb92ed9f3ebeb71690ae8eeefa1404
Al Zaudtke <azaudtke@jjkeller.com>
2019-12-27 15:28:37 -0500

    update path of favicon to work with sub folders in url

--- components/Meta.js
+++ components/Meta.js
@@ -8,7 +8,7 @@ const Meta = () => (
     <title key="title">THAT Conference - Wisconsin Dells, WI</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
-    <link rel="icon" type="image/png" href="favicon.png" />
+    <link rel="icon" type="image/png" href="/favicon.png" />
 
     <link rel="stylesheet" type="text/css" href="/nprogress.css" />
     <link rel="stylesheet" href="https://use.typekit.net/tyl7iga.css" />
