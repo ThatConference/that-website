3da6ce4688d2e29c79a8ba1951b9091040a675e4
Keith Burnell <keith@kburnell.com>
2019-12-28 13:17:49 -0600

    wip

--- pages/wi/session/submit/details.js
+++ pages/wi/session/submit/details.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Router from 'next/router';
 
 import ContentSection from '../../../../components/shared/ContentSection';
 import togglePage from '../../../../utilities/togglePage';
@@ -7,18 +8,31 @@ import togglePage from '../../../../utilities/togglePage';
 import Header from '../../../../components/Session/Submit/Header';
 import Details from '../../../../components/Session/Submit/Details';
 
+import { useFetchUser } from '../../../../lib/user';
+
 const SessionDetails = ({ featureKeyword }) => {
-  return (
-    <div>
-      <Head>
-        <title key="title">Session Submission: Details - THAT Conference</title>
-      </Head>
-      <ContentSection forForm>
-        <Header title="Session Details" currentStep="1" />
-        <Details featureKeyword={featureKeyword} />
-      </ContentSection>
-    </div>
-  );
+  const { user, loading } = useFetchUser();
+  React.useEffect(() => {
+    if (!loading && !user) {
+      Router.push('/api/login');
+    }
+  });
+  if (user) {
+    return (
+      <div>
+        <Head>
+          <title key="title">
+            Session Submission: Details - THAT Conference
+          </title>
+        </Head>
+        <ContentSection forForm>
+          <Header title="Session Details" currentStep="1" />
+          <Details featureKeyword={featureKeyword} />
+        </ContentSection>
+      </div>
+    );
+  }
+  return null;
 };
 
 export default togglePage(SessionDetails);
