c6cf428733ed8ae8105d6f12f08ffcaa39d90934
Keith Burnell <keith@kburnell.com>
2019-12-28 11:03:25 -0600

    wip

--- components/shared/Stepper.js
+++ components/shared/Stepper.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  margin-top: -5rem;
+  margin-top: -10rem;
+  margin-bottom: 4rem;
 `;
 
 const Header = styled.h5`
