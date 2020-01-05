57ca9fa7f31fd559de260c7e02e4de880c2a40b9
Aaron Douglas <astralbodies@gmail.com>
2020-01-05 09:10:41 -0600

    Update Counselor Commitments to more honest about attendance

--- components/CounselorAgreement/Commitments.js
+++ components/CounselorAgreement/Commitments.js
@@ -9,7 +9,7 @@ const Agreement = () => {
           You agree to the Code of Conduct and a zero tolerance policy for any
           sort of harassment.
         </li>
-        <li>You’ll attend the entirety of the conference.</li>
+        <li>You’ll try to attend the entirety of the conference.</li>
         <li>
           You’ll promise to meet new people and really discover what it means to
           be a part of THAT Conference.
