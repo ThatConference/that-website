402b647f92f5e9a01194e6ada15f99379af5f684
Clark Sell <clark.sell@csell.net>
2020-01-06 16:03:05 -0600

    updates regex and adds placeholer text

--- utilities/validation.js
+++ utilities/validation.js
@@ -1,5 +1,5 @@
 export const RegularExpressions = {
-  phoneRegExp: /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+  phoneRegExp: /^\+[1-9]\d{1,14}$/,
   urlRegExp: /^((ftp|http|https):\/\/)?www\.([A-z]+)\.([A-z]{2,})/,
 };
 
