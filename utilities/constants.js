74de7a22e3e8658e6b295be6d27be4409cde916d
Sara Gibbons <saragibby@gmail.com>
2020-01-11 10:31:55 -0500

    add image upload + profile cleanup

--- utilities/constants.js
+++ utilities/constants.js
@@ -1,3 +1,6 @@
+import * as Yup from 'yup';
+import { RegularExpressions } from './validation';
+
 // eslint-disable-next-line import/prefer-default-export
 export const sessionConstants = {
   SessionFors: [
@@ -57,3 +60,117 @@ export const sessionConstants = {
     { value: 'CANCELLED', label: 'Canceled' },
   ],
 };
+
+export const memberConstants = {
+  // commented out some links until we get icons
+  linkTypes: {
+    // devTo: "DEV_TO",
+    // dribbble: "DRIBBBLE",
+    facebook: 'FACEBOOK',
+    github: 'GITHUB',
+    instagram: 'INSTAGRAM',
+    linkedin: 'LINKEDIN',
+    medium: 'MEDIUM',
+    // stackOverflow: "STACK_OVERFLOW",
+    // tictok: "TICTOK",
+    // twitch: "TWITCH",
+    twitter: 'TWITTER',
+    website: 'WEBSITE',
+    youtube: 'YOUTUBE',
+  },
+
+  validationRules: {
+    createOnly: {
+      acceptedCodeOfConduct: Yup.bool().required('Field must be checked'),
+      acceptedTermsOfService: Yup.bool().required('Field must be checked'),
+      isOver13: Yup.bool().required('Field must be checked'),
+    },
+    contactInfo: {
+      city: Yup.string().nullable(),
+      company: Yup.string().nullable(),
+      firstName: Yup.string()
+        .min(3, 'Must be at least 3 characters')
+        .required('Required'),
+      email: Yup.string()
+        .email('Invalid email address')
+        .required('Required'),
+      country: Yup.string().nullable(),
+      jobTitle: Yup.string().nullable(),
+      lastName: Yup.string()
+        .min(3, 'Must be at least 3 characters')
+        .required('Required'),
+      mobilePhone: Yup.string()
+        .matches(RegularExpressions.phoneRegExp, 'Phone number is not valid')
+        .nullable(),
+      profileSlug: Yup.string().required('Required'),
+      state: Yup.string().nullable(),
+    },
+    onlinePresence: {
+      facebook: Yup.string().url('Invalid URL'),
+      github: Yup.string().url('Invalid URL'),
+      instagram: Yup.string().url('Invalid URL'),
+      linkedin: Yup.string().url('Invalid URL'),
+      slack: Yup.string(),
+      twitter: Yup.string().url('Invalid URL'),
+      website: Yup.string().url('Invalid URL'),
+    },
+    image: { profileImage: Yup.mixed().required() },
+    bio: {
+      bio: Yup.string()
+        .min(10, 'Must be at least 10 characters')
+        .required(),
+    },
+  },
+};
+
+export const socialConstants = {
+  thatSocialLinks: Object.freeze({
+    facebook: 'https://www.facebook.com/ThatConference/',
+    instagram: 'https://www.instagram.com/thatconference/',
+    twitter: 'https://twitter.com/ThatConference',
+    medium: 'https://medium.com/that-conference',
+    youtube: 'https://www.youtube.com/thatconference/',
+  }),
+
+  socialIcons: {
+    facebook: {
+      icon: {
+        name: 'facebook',
+        width: 12,
+        height: 24,
+      },
+      className: 'is-bigger',
+    },
+    instagram: {
+      icon: {
+        name: 'instagram',
+        width: 24,
+        height: 24,
+      },
+      className: 'is-bigger',
+    },
+    twitter: {
+      icon: {
+        name: 'twitter',
+        width: 32,
+        height: 26,
+      },
+    },
+    medium: {
+      icon: {
+        name: 'medium',
+        width: 24,
+        height: 19,
+      },
+    },
+    youtube: {
+      icon: {
+        name: 'youtube',
+        width: 90,
+        height: 63,
+      },
+    },
+  },
+};
+
+export default { sessionConstants, memberConstants, socialConstants };
