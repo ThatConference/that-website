40b4eef18cb4ebee230005d1cb60ffd4ba71cade
Keith Burnell <keith@kburnell.com>
2020-01-18 14:06:24 -0600

    revert

--- components/Header/Header.spec.js
+++ components/Header/Header.spec.js
@@ -1,13 +1,13 @@
-// // import React from 'react';
-// // import { shallow } from 'enzyme';
-// // import Header from '.';
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import Header from '.';
 
-// describe('Header', () => {
-//   test('renders', () => {
-//     // commenting out for now as need to fix provider/store with connect is specs
-//     // const wrapper = shallow(<Header />);
-//     // expect(wrapper.exists()).toBe(true);
+describe('Header', () => {
+  test('renders', () => {
+    // commenting out for now as need to fix provider/store with connect is specs
+    // const wrapper = shallow(<Header />);
+    // expect(wrapper.exists()).toBe(true);
 
-//     expect(true).toBe(true);
-//   });
-// });
+    expect(true).toBe(true);
+  });
+});
