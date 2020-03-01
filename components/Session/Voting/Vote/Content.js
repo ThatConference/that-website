1278ae80a65e95303cc56bc59c519044c90e55d4
Sara Gibbons <saragibby@gmail.com>
2020-02-29 21:06:01 -0500

    add framer and use animation instead of toast

--- components/Session/Voting/Vote/Content.js
+++ components/Session/Voting/Vote/Content.js
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 import parse from 'html-react-parser';
 import { useMutation } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
-import ButterToast, { Cinnamon, POS_TOP, POS_RIGHT } from 'butter-toast';
+import { motion } from 'framer-motion';
 import { GlobalHotKeys } from 'react-hotkeys';
 import Icon from '../../../shared/Icon';
 import { below } from '../../../../utilities';
@@ -80,6 +80,22 @@ const ThumbsUpIcon = styled(Icon)`
   }
 `;
 
+const SavingDiv = styled.div`
+  background-color: rgba(255, 255, 255, 0.85);
+  height: calc(100% + 2rem);
+  position: absolute;
+  width: calc(100% + 2rem);
+  z-index: 999;
+  top: -1rem;
+  left: -1rem;
+  align-items: center;
+  justify-content: center;
+
+  .trees {
+    width: 15rem;
+  }
+`;
+
 const CAST_VOTE = gql`
   mutation castVote($eventId: ID!, $vote: VoteInput!) {
     sessions {
@@ -94,26 +110,32 @@ const CAST_VOTE = gql`
   }
 `;
 
-const Content = ({ session }) => {
+const icon = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: '#F74646',
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: '#F74646',
+  },
+};
+
+const Content = ({ session, increaseVoteCount }) => {
   const [notes, setNotes] = useState('');
   const [vote, setVote] = useState(null);
+  const [submitting, setSubmitting] = useState(false);
 
   const voteComplete = () => {
-    console.log('vote mutation completed');
-    // ButterToast.dismiss(toastId);
-    // ButterToast.raise({
-    //   sticky: false,
-    //   content: (
-    //     <Cinnamon.Crisp
-    //       scheme={Cinnamon.Crisp.SCHEME_BLUE}
-    //       content={() => <div>Vote successfully cast!</div>}
-    //       title="Vote"
-    //     />
-    //   ),
-    // });
-    // setValue(value + 1);
-    // setNotes('');
-    // window.scrollTo(0, 0);
+    increaseVoteCount();
+    setVote(null);
+    setNotes('');
+    window.scrollTo(0, 0);
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 1500);
   };
 
   const [castVote] = useMutation(CAST_VOTE, {
@@ -123,14 +145,8 @@ const Content = ({ session }) => {
     },
   });
 
-  // const { totalSubmitted } = root;
-  // const totalRemaining = root.unVoted.length - value;
-  // const totalVotedOn = totalSubmitted - totalRemaining;
-
-  // const session = root.unVoted ? root.unVoted[value] : null;
-
   const submitVote = yesVote => {
-    console.log('SUBMIT VOTE');
+    setSubmitting(true);
     setVote(yesVote);
     const queryVariables = {
       eventId: process.env.CURRENT_EVENT_ID,
@@ -155,7 +171,49 @@ const Content = ({ session }) => {
     const { longDescription, takeaways, title } = session;
 
     return (
-      <>
+      <div style={{ position: 'relative' }}>
+        <SavingDiv style={{ display: submitting ? 'flex' : 'none' }}>
+          <div style={{ height: '15rem' }}>
+            {submitting && (
+              <motion.svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 200 200"
+                className="trees"
+              >
+                <motion.path
+                  d="M25.74 9.24 7.15 27.83 0 34.98 7.15 42.13 25.74 60.73 32.89 53.58 14.3 34.98 32.89 16.39 25.74 9.24z"
+                  variants={icon}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{
+                    default: { duration: 2, ease: 'easeInOut' },
+                    fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+                  }}
+                />
+                <motion.path
+                  d="M151.62,50.67,136.15,26h5.71L125.46,0l-16.4,26h5.71L100,49.58,85.21,26h5.71L74.52,0,58.12,26h5.72L48.38,50.67h5.71L38,76.47H67.89V87.39H81.18V76.46h37.64V87.38h13.29V76.46H162l-16.11-25.8ZM95,50.67h10l-5,8Z"
+                  variants={icon}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{
+                    default: { duration: 2, ease: 'easeInOut' },
+                    fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+                  }}
+                />
+                <motion.path
+                  d="M192.85 27.83 174.26 9.24 167.11 16.39 185.7 34.98 167.11 53.58 174.26 60.73 192.85 42.13 200 34.98 192.85 27.83z"
+                  variants={icon}
+                  initial="hidden"
+                  animate="visible"
+                  transition={{
+                    default: { duration: 2, ease: 'easeInOut' },
+                    fill: { duration: 2, ease: [1, 0, 0.8, 1] },
+                  }}
+                />
+              </motion.svg>
+            )}
+          </div>
+        </SavingDiv>
         <GlobalHotKeyStyled keyMap={keyMap} handlers={handlers} />
 
         <h3>{title}</h3>
@@ -206,15 +264,7 @@ const Content = ({ session }) => {
             </ThumbRow>
           </SideColumn>
         </SessionDetails>
-
-        <ButterToast
-          className="that-toast"
-          position={{
-            vertical: POS_TOP,
-            horizontal: POS_RIGHT,
-          }}
-        />
-      </>
+      </div>
     );
   }
 
