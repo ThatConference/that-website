921fb99f063af0a7be74bcd39cda6d92fe0dcd4b
Clark Sell <clark.sell@csell.net>
2020-04-13 13:11:56 -0500

    april update blog post

--- pages/blog.js
+++ pages/blog.js
@@ -30,6 +30,7 @@ const blog = () => (
       </Grid>
     </ContentSection>
     <BlogList>
+      <BlogListItem slug="april-2020-update" />
       <BlogListItem slug="womens-day-2020" />
       <BlogListItem slug="2020-submission-fields" />
       <BlogListItem slug="celebrating-black-history-month" />
