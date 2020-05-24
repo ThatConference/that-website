116a9cd5b9b1d6576e13eb83cdb22fd6fea74597
Keith Burnell <keith@kburnell.com>
2020-05-24 11:11:22 -0500

    undid change to blog

--- components/RootHomePage/Blog.js
+++ components/RootHomePage/Blog.js
@@ -2,15 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import ContentSection from '../shared/ContentSection';
 
-const Container = styled.div`
-  background-color: red;
-`;
-
 const Blog = ({ className }) => {
   return (
-    <Container className={className}>
+    <ContentSection className={className}>
       <p>this is root blog section</p>
-    </Container>
+    </ContentSection>
   );
 };
 
