8030554889a11ef8f45bd70382eba1b3dcc977c8
Keith Burnell <keith@kburnell.com>
2020-06-10 04:37:07 -0500

    wip

--- components/OneDay/Hero.js
+++ components/OneDay/Hero.js
@@ -5,10 +5,11 @@ import styled from 'styled-components';
 import { above, below } from '../../utilities';
 import HeroContainer from '../shared/HeroContainer';
 import YouTubeVideo from '../shared/YouTubeVideo';
+import ContentSection from '../shared/ContentSection';
 
 const TitleBlock = styled.div`
   position: absolute;
-  top: 3rem;
+  top: 1rem;
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -30,46 +31,21 @@ const StyledH1 = styled.h1`
 `;
 
 const ResponsiveYouTube = styled(YouTubeVideo)`
-  width: 100%;
-  min-height: 40rem;
-
-  ${below.larger`
-    min-width: 50rem;
-    min-height: 35rem;
-    display: inline-grid;
-    max-width: 55rem;
-    margin: auto;
-  `};
-
-  ${below.med`
-    min-width: 40rem;
-    max-width: 45rem;
-    min-height: 30rem;
-  `};
-
-  ${below.small`
-    min-width: 40rem;
-    min-height: 50rem;
-  `};
+  height: 40rem;
+  width: 60rem;  
+  margin: auto;
 `;
 
-const twoColBp = 'larger';
-
-const Main = styled.div`
-  display: flex;
-  flex-direction: row;
-  //margin: auto;
-  justify-content: center;
-
-  ${below[twoColBp]`
-    flex-direction: column;
-  `};
+const Main = styled(ContentSection)`
+  background: unset;
+  background-color: transparent;
+  margin: auto;
+  padding-top: 0;
 `;
 
 const VIDEO_ID = 'R67gLABGvVE';
 
 const Hero = ({ className, event }) => {
-  // temporary
   event.theme.heroSlug = 'landing_hero.jpg';
   return (
     <HeroContainer
@@ -82,17 +58,10 @@ const Hero = ({ className, event }) => {
           <StyledH1 className="font-light">{event.slogan}</StyledH1>
         </HeadingBlock>
         <Main>
-          <ResponsiveYouTube
-            videoId={VIDEO_ID}
-            autoplay={0}
-            rel={0}
-            modest={1}
-            containerHeight="40rem"
-            containerWidth="100%"
-          />
-        </Main>
-      </TitleBlock>
-    </HeroContainer>
+          <ResponsiveYouTube videoId={VIDEO_ID} />
+         </Main>
+       </TitleBlock>
+     </HeroContainer>
   );
 };
 
