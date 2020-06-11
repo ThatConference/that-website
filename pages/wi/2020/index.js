389eb2fb062ffae197f07d12ad1612701a16c3fd
Sara <saragibby@gmail.com>
2020-06-10 22:35:47 -0400

    add one day event ticket section

--- pages/wi/2020/index.js
+++ pages/wi/2020/index.js
@@ -6,8 +6,51 @@ import React from 'react';
 import RootLayout from '../../../components/layouts/Root';
 import OneDayEvent from '../../../components/EventPages/OneDayEvent';
 
+const TICKETS = [
+  {
+    name: 'Camper',
+    description:
+      'some description of what a camper is that makes it sound super cool',
+    imagePath: '/images/tickets/2020/camper.png',
+    basePrice: '0',
+    benefits: ['benefit 1', 'benefit 2', 'benefit 3', 'benefit 4'],
+    ticketUrl:
+      'https://ti.to/that-conference/that-conference-2020?odor261sw70=1',
+  },
+  {
+    name: 'Patron',
+    description:
+      'some description of what a patron is that makes it sound super cool',
+    imagePath: '/images/tickets/2020/patron.png',
+    basePrice: '25',
+    benefits: ['benefit 1', 'benefit 2', 'benefit 3', 'benefit 4'],
+    ticketUrl:
+      'https://ti.to/that-conference/that-conference-2020?grocntbbmo=1',
+  },
+  {
+    name: 'Partner',
+    description:
+      'some description of what a partner is that makes it sound super cool',
+    imagePath: '/images/tickets/2020/partner.png',
+    basePrice: '250',
+    benefits: ['benefit 1', 'benefit 2', 'benefit 3', 'benefit 4'],
+    ticketUrl:
+      'https://ti.to/that-conference/that-conference-2020?y32kfajepau=1',
+  },
+  {
+    name: 'Corporate Partner',
+    description:
+      'some description of what a corporate partner is that makes it sound super cool',
+    imagePath: '/images/tickets/2020/corporate-partner.png',
+    basePrice: '1000',
+    benefits: ['benefit 1', 'benefit 2', 'benefit 3', 'benefit 4'],
+    ticketUrl:
+      'https://ti.to/that-conference/that-conference-2020?dhsgaihiwps=1',
+  },
+];
+
 const Index = ({ eventSlug }) => {
-  return <OneDayEvent eventSlug={eventSlug.substring(1)} />;
+  return <OneDayEvent eventSlug={eventSlug.substring(1)} tickets={TICKETS} />;
 };
 
 Index.Layout = RootLayout;
