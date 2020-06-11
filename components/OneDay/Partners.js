4879a22e364b151be4e588514926dea3904a8f69
Sara <saragibby@gmail.com>
2020-06-10 20:02:49 -0400

    add one day parters section

--- components/OneDay/Partners.js
+++ components/OneDay/Partners.js
@@ -2,22 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import ContentSection from '../shared/ContentSection';
+import TopPartners from '../shared/TopPartners';
 
-const Partners = ({ className }) => {
+const PARTNER_LEVELS_TO_DISPLAY = ['CORPORATE_PARTNER', 'PARTNER'];
+
+const Partners = ({ partners }) => {
+  const topPartners = partners.filter(partner =>
+    PARTNER_LEVELS_TO_DISPLAY.includes(partner.level),
+  );
+
   return (
-    <ContentSection className={className}>
-      <p>this is the one day partners</p>
+    <ContentSection>
+      <TopPartners title="Event Brought To You By" partners={topPartners} />
     </ContentSection>
   );
 };
 
 Partners.propTypes = {
-  className: PropTypes.string,
-  event: PropTypes.shape({}).isRequired,
+  partners: PropTypes.shape([]).isRequired,
 };
 
-Partners.defaultProps = {
-  className: '',
-};
+Partners.defaultProps = {};
 
 export default styled(Partners)``;
