855fa10dad0af82dfafd69e030d10bd38f6eb924
Brett Slaski <brett@thatconference.com>
2020-06-15 12:23:45 -0500

    Draft ticket text on wi 2020 page

--- pages/wi/2020/index.js
+++ pages/wi/2020/index.js
@@ -10,10 +10,10 @@ const TICKETS = [
   {
     name: 'Camper',
     description:
-      'some description of what a camper is that makes it sound super cool',
+      'Get access to the event with no base contribution to THAT community.',
     imagePath: '/images/tickets/2020/camper.png',
     basePrice: '0',
-    benefits: ['benefit 1', 'benefit 2', 'benefit 3', 'benefit 4'],
+    benefits: ['Access to the event'],
     buttonColor: 'tertiary',
     ticketUrl:
       'https://ti.to/that-conference/that-conference-2020?odor261sw70=1',
@@ -21,10 +21,14 @@ const TICKETS = [
   {
     name: 'Patron',
     description:
-      'some description of what a patron is that makes it sound super cool',
+      'Access to the event with a $25 minimum contribution to THAT community.',
     imagePath: '/images/tickets/2020/patron.png',
     basePrice: '25',
-    benefits: ['benefit 1', 'benefit 2', 'benefit 3', 'benefit 4'],
+    benefits: [
+      'Access to the event',
+      'Unique digital merit badge on THAT profile',
+      '10% discout at THAT Store',
+    ],
     buttonColor: 'primary',
     ticketUrl:
       'https://ti.to/that-conference/that-conference-2020?grocntbbmo=1',
@@ -32,21 +36,31 @@ const TICKETS = [
   {
     name: 'Partner',
     description:
-      'some description of what a partner is that makes it sound super cool',
+      'Individual Partnership for individuals, sole proprietors and start-ups with less than 1M in ARR.',
     imagePath: '/images/tickets/2020/partner.png',
     basePrice: '250',
-    benefits: ['benefit 1', 'benefit 2', 'benefit 3', 'benefit 4'],
+    benefits: [
+      'Access to the event',
+      'Listed on event as a partner',
+      'Linked logo and social media',
+      '20% discount at THAT Store',
+    ],
     buttonColor: 'teal',
     ticketUrl:
       'https://ti.to/that-conference/that-conference-2020?y32kfajepau=1',
   },
   {
-    name: 'Corporate Partner',
-    description:
-      'some description of what a corporate partner is that makes it sound super cool',
+    name: 'Corporate Partnership',
+    description: 'Corporate partner ticket',
     imagePath: '/images/tickets/2020/corporate-partner.png',
     basePrice: '1000',
-    benefits: ['benefit 1', 'benefit 2', 'benefit 3', 'benefit 4'],
+    benefits: [
+      'Access to the event',
+      'Listed on event as a corporate partner',
+      'Linnked logo and social media',
+      'Up to three job posting on THAT Conference job board',
+      '20% discount at THAT Store',
+    ],
     buttonColor: 'purple',
     ticketUrl:
       'https://ti.to/that-conference/that-conference-2020?dhsgaihiwps=1',
