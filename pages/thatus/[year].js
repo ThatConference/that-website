86bdd23272dd4e7e05bf201ae33a0d0a6c0a6f33
Clark Sell <clark.sell@csell.net>
2020-12-14 11:45:42 -0600

    enables that.us redirection

---
+++ pages/thatus/[year].js
@@ -0,0 +1,7 @@
+import redirect from '../../components/shared/Redirect';
+
+/**
+ * TO DO: hardcoded for now, eventually be great to have this
+ * dynamically pull current event for location
+ */
+export default redirect('https://that.us?utm_source=thatconference.com');
